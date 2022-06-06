87ef0eca38f754db0ed4e366015ced4b7b241efd
Ramon Isaac Pizaña Espinosa <84466653+ramonpizana@users.noreply.github.com>
2022-06-06 17:22:50 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  
+provider "azurerm" {
+  features {}
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+  backend "remote" {
+    organization = "tf-cloud-rpe"
+
+    workspaces {
+      name = "test-tfc"
+    }
+  }
+}
+
+
+data "azurerm_resource_group" "rg" {
+  name = "ramon-pizana"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
